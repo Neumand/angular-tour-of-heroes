c98d848d47c384dccae67991314b0460b4ab1711
David Neuman <david.m.neuman@gmail.com>
2019-05-28 14:47:59 -0400

    Create and display list of heroes. Add click event handler to update the details template with the selected hero

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,17 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice', power: 'Kills with kindness' },
+  { id: 12, name: 'Narco', power: 'Generates illicit drugs at will' },
+  { id: 13, name: 'Bombasto', power: 'Explosions and bombs and stuff' },
+  { id: 14, name: 'Celeritas', power: 'Rejuvenative powers' },
+  { id: 15, name: 'Magneta', power: 'Like Magneto, but a badass woman' },
+  { id: 16, name: 'RubberMan', power: 'Extremely elastic and mobile' },
+  {
+    id: 17,
+    name: 'Dynama',
+    power: `Bombasto and Magma's daughter, lot of fire and explosions`,
+  },
+  { id: 19, name: 'Magma', power: 'Fire and flames and hot stuff' },
+  { id: 20, name: 'Tornado', power: 'Pretty self-explanatory' },
+];
