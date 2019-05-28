1157d3cfb80b5badb8357f8a8966f3a6c77f31a5
David Neuman <david.m.neuman@gmail.com>
2019-05-28 15:28:22 -0400

    Create hero service file. Add observable stream for getting the list of heroes. HeroesComponent then subscribes to the stream

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,14 +8,19 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
-  selectedHero: Hero;
+  constructor(private heroService: HeroService) {}
 
-  constructor() {}
+  heroes: Hero[];
+  selectedHero: Hero;
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getHeroes();
+  }
 }
